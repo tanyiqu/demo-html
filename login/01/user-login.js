5257f9bdfc200e16b8aafa62e4219fec7492a33e
Tanyiqu <1953649096@qq.com>
2021-01-06 13:38:30 +0800

    实现登录页面的点击效果

--- login/01/user-login.js
+++ login/01/user-login.js
@@ -12,49 +12,21 @@ $(function () {
  * 加载事件
  */
 function loadEvents() {
-    // 菜单切换
-    switchDivTab([
-        new DivTab('#userLogin', '.user'),
-        new DivTab('#adminLogin', '.admin')
-    ]);
 
-    // 管理员登录
-    $('#adminLoginForm').submit(() => {
-        simpleLogin({
-            username: $('#adminUsername').val(),
-            password: $('#adminPassword').val(),
-            type: 3
-        }, (data) => {
-            // 存储账号密码到 localStorage中
-            console.log(data);
-            localStorage.setItem('course-web-curr-admin-username', data.data['adminAccount']);
-            localStorage.setItem('course-web-curr-admin-password', data.data['adminPwd']);
-            // 跳转到管理页面
-            window.location.href = `admin.html?sign=${md5(data.data['adminAccount'] + data.data['adminPwd'])}`;
-        });
-        return false;
+    // 用户登录
+    $('#userLogin').click(() => {
+        $('#userLogin').addClass('active');
+        $('#adminLogin').removeClass('active');
+        $('#userLoginForm').show();
+        $('#adminLoginForm').hide();
     });
 
-    // 教师登录
-    $('#userLoginForm').submit(() => {
-        let username = $('#teacherUsername').val();
-        let password = $('#teacherPassword').val();
-        simpleLogin({
-            username: username,
-            password: password,
-            type: 2
-        }, (data) => {
-            // 存储账号密码到 localStorage中
-            localStorage.setItem('course-web-curr-teacher-username', data.data['tno']);
-            localStorage.setItem('course-web-curr-teacher-password', data.data['pwd']);
-            localStorage.setItem('course-web-curr-teacher-name', data.data['name']);
-            localStorage.setItem('course-web-curr-teacher-avatar', data.data['avatar']);
-            console.log(localStorage['course-web-curr-teacher-username']);
-            console.log(localStorage['course-web-curr-teacher-password']);
-            // 跳转到教师页面
-            window.location.href = 'teacher.html?sign=' + md5(localStorage['course-web-curr-teacher-username'] + localStorage['course-web-curr-teacher-password']);
-        });
-        return false;
+    // 管理员登录
+    $('#adminLogin').click(() => {
+        $('#adminLogin').addClass('active');
+        $('#userLogin').removeClass('active');
+        $('#adminLoginForm').show();
+        $('#userLoginForm').hide();
     });
 
 }
